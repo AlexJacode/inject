189d363e03a4ae3faaab743da1f740a4524674a6
Alex <108822756+AlexJacode@users.noreply.github.com>
2025-07-18 23:25:51 -0400

    Update inject.cpp

--- inject.cpp
+++ inject.cpp
@@ -1,27 +1,38 @@
 #include <iostream> 
 #include <fstream> 
-#include <cstdlib> 
+#include <string> 
+#include <cstdio>  
 
 int main(int argc, char *argv[]) { 
-        if (argc < 2) { std::cout << "No Arguments Detected \n"; return -3; } 
-        if (argc == 2 && argv[1] == "INPUTcin") { 
-                // ; 
+        if (argc == 1 || argv[1] == "--help") { 
+                std::cout << "INJECT \n\n" << "Usage: inject [File] [Line Number] [Content] \n";
+                return -3; // Help Command 
         } 
-        else if (argc >= 3) {
-                std::string ln = "/usr/local/bin/"; 
-                ln+=argv[1]; 
-                std::ofstream file(ln.c_str()); 
-                file << "#!/bin/sh\n"; 
-                for (short i = 2; i<argc; i++) { 
-                        file << argv[i]; 
-                        file << " "; 
+        else if (argc < 4) { 
+                std::cout << "Invalid, Not Enough Arguments; Run --help For More Info. \n"; 
+                return -2; // Not Enough Args 
+        } 
+        std::ifstream file(argv[1]);
+        std::string* pHemp = new std::string("temp_"); 
+        *pHemp+=argv[1]; 
+        const char* THEMP = (*pHemp).c_str(); 
+        delete pHemp; 
+        std::ofstream tfile(THEMP); 
+        long num = std::stoi(argv[2]); 
+        std::string line; 
+        long li = 0; 
+        while (std::getline(file, line)) { 
+                if (li == num) { 
+                        for (short i=3;i<argc;i++) { tfile << argv[i]; tfile << " "; } 
                 } 
-                file << "\"$@\""; 
-                file.close(); 
-                std::string cmdline = (std::string) "sudo chmod +x /usr/local/bin/" + (std::string) argv[1];
-
-                const char *cmd = (const char*) cmdline.c_str(); 
-                std::system(cmd); 
+                else { 
+                        tfile << line; 
+                } 
+                li++; 
         } 
+        file.close(); 
+        tfile.close(); 
+        std::remove(argv[1]); 
+        std::rename(THEMP, argv[1]); 
         return 0; 
 } 
